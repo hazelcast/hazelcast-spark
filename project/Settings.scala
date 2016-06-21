1af72253161c056e0f47453760fda79c2db3c8b0
Emin Demirci <emindemirci@gmail.com>
2016-06-21 12:53:51 +0300

    added license, scm, developer and issue management info to the generated pom

--- project/Settings.scala
+++ project/Settings.scala
@@ -17,7 +17,34 @@ object Settings {
     resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
     resolvers += Resolver.mavenLocal,
     parallelExecution in Test := false,
-    test in assembly := {}
+    test in assembly := {},
+    pomExtra := <licenses>
+      <license>
+        <name>The Apache Software License, Version 2.0</name>
+        <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+        <distribution>repo</distribution>
+      </license>
+    </licenses>
+      <scm>
+        <connection>scm:git:git://github.com/hazelcast/hazelcast-spark.git</connection>
+        <developerConnection>scm:git:git@github.com:hazelcast/hazelcast-spark.git</developerConnection>
+        <url>https://github.com/hazelcast/hazelcast-spark/</url>
+      </scm>
+      <developers>
+        <developer>
+          <id>eminn</id>
+          <name>emin demirci</name>
+          <email>emin@hazelcast.com</email>
+        </developer>
+      </developers>
+      <issueManagement>
+        <system>Github</system>
+        <url>https://github.com/hazelcast/hazelcast-spark/issues</url>
+      </issueManagement>
+      <organization>
+        <name>Hazelcast, Inc.</name>
+        <url>http://www.hazelcast.com/</url>
+      </organization>
   )
 
 }
