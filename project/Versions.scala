05ed1ae5419f0f6dd38a2bf2548718fe36a59300
VEINHORN <b.korogvich@gmail.com>
2016-07-07 22:06:17 +0300

    updated dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,5 +2,6 @@ object Versions {
   val hazelcastVersion = "3.7-SNAPSHOT"
   val sparkVersion = "1.6.1"
   val jcacheVersion = "1.0.0"
-  val junitVersion = "4.11"
+  val junitVersion = "4.12"
+  val junitInterfaceVersion = "0.11"
 }
